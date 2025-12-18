888ca972692c9d5b46b5e405b658e8eb445c7a9a
Satyam Choubey <satyamchoubey789@gmail.com>
2025-12-19 01:08:22 +0530

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/apple-touch-icon.png",
   },
 };
 
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,6 +14,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: "https://www.servigrow.com/contact",
       lastModified: new Date(),
     },
-    // Add more pages dynamically if needed
   ];
 }
